d673eb324404b106ecc672ab5f1020e2f7fcee14
Anthony Harper <gaiansentience@gmail.com>
2022-05-11 17:37:19 -0600

    unit test scripts for show_customer_event_tickets and show_customer_event_tickets_by_email

--- test-scripts/unit_tests/events.test.05.01.05.01.show_customer_event_tickets.sql
+++ test-scripts/unit_tests/events.test.05.01.05.01.show_customer_event_tickets.sql
@@ -2,21 +2,18 @@
 select
 *  
 from 
-events_report_api.show_customer_event_tickets(517, 67);
-
-select
-*  
-from 
-events_report_api.show_customer_event_tickets(919, 67);
+events_report_api.show_customer_event_tickets(2379, 27);
 
 
   
-  /*
+  
+  --find a customer with tickets from a reseller
   select tg.event_id, ts.customer_id
   from ticket_groups tg join ticket_sales ts on tg.ticket_group_id = ts.ticket_group_id
   where ts.reseller_id is not null;
   
+  --find a customer with tickets from the venue
   select tg.event_id, ts.customer_id
   from ticket_groups tg join ticket_sales ts on tg.ticket_group_id = ts.ticket_group_id
-  where ts.reseller_id is not null;
-  */
+  where ts.reseller_id is null;
+  
